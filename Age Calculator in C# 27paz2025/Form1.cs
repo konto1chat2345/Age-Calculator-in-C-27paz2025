dfff8fc66ae7bc31508e86be1732c18d31d5898d
Leon <konto1chat2345@email.com>
2025-10-27 21:23:30 +0100

    Add project files.

---
+++ Age Calculator in C# 27paz2025/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_Calculator_in_C__27paz2025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
